54b33e72d39e7a6909bc81c5fc5beb3dcdb211ed
Кирилл <89464316+Peachoopeado@users.noreply.github.com>
2024-01-23 23:28:32 +0300

    Структуры страниц

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ ESHOP.use(express.static('public'))
 ESHOP.get('/', (req, res) => {
     res.render('index')
 })
+ESHOP.get('/', (req, res) => {
+    res.render('index')
+})
+ESHOP.get('/about', (req, res) => {
+    res.render('about')
+})
+ESHOP.get('/partners', (req, res) => {
+    res.render('partners')
+})
+ESHOP.get('/shop', (req, res) => {
+    res.render('shop')
+})
+ESHOP.get('/contacts', (req, res) => {
+    res.render('contacts')
+})
+
 
 ESHOP.listen(3000, ()=> {
     console.log('Завёлся: http://localhost:3000')
